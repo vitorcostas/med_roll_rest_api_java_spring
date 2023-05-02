a850c38b7ae1924579732f6b36102e90956b552c
Vitor Costa Salem <vitor635@hotmail.com>
2023-05-02 08:57:29 -0300

    Repositor interface

--- api/src/main/java/med/voll/api/medico/Medico.java
+++ api/src/main/java/med/voll/api/medico/Medico.java
@@ -13,7 +13,6 @@ import med.voll.api.endereco.Endereco;
 @AllArgsConstructor
 @EqualsAndHashCode(of="id")
 public class Medico {
-
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
@@ -26,5 +25,11 @@ public class Medico {
     @Embedded
     private Endereco endereco;
 
-
+    public Medico(DadosCadastroMedico dados) {
+        this.nome = dados.nome();
+        this.email = dados.email();
+        this.crm = dados.crm();
+        this.especialidade = dados.especialidade();
+        this.endereco = new Endereco(dados.endereco());
+    }
 }
